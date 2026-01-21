fd615c13eb28e199031334b91cadd6e9448d526c
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2026-01-21 11:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (10.70%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        vector<int> F(n+1, -1);
+        F[0] = 0;
+        F[1] = 1;
+        for(int i=2; i<=n; i++){
+            F[i] = F[i-2] + F[i-1];
+        }
+        return F[n];
+    }
+};
