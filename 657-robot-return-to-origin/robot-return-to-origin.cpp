0efeccb50dddc271e5f2c2ac83fffd68d847dd34
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2026-04-05 07:15:37 +0530

    Time: 7 ms (10.20%) | Memory: 10.4 MB (47.26%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        bool ans = false;
+
+        for(char m : moves){
+            if(m == 'U'){
+                y++;
+            }else if(m == 'D'){
+                y--;
+            }else if(m == 'R'){
+                x++;
+            }else if(m == 'L'){
+                x--;
+            }
+        }
+
+        if(x == 0 && y == 0){
+            ans = true;
+        }
+
+        return ans;
+    }
+};
