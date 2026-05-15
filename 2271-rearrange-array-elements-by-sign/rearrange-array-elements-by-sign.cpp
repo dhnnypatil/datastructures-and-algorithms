bb1cd038340cffa951c334802151f39c4d402512
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2026-05-15 09:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 128.4 MB (54.33%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> temp1;
-        vector<int> temp2;
-        vector<int> ans;
-
-        for(int i=0; i<n; i++){
-            if(nums[i] > 0) temp1.push_back(nums[i]);
-        }
-        for(int i=0; i<n; i++){
-            if(nums[i] < 0) temp2.push_back(nums[i]);
-        }
-
-        for(int i=0; i<temp1.size(); i++){
-            ans.push_back(temp1[i]);
-            ans.push_back(temp2[i]);
-        }
-
-        return ans;
+        int n=nums.size();
+       vector<int>ans(n,0);
+       int evenIndex=0;
+        int oddIndex=1;
+       for(int i=0;i<n;i++){
+        if(nums[i]>=0){
+            ans[evenIndex]=nums[i];
+            evenIndex+=2;
+       }
+       else{
+        ans[oddIndex]=nums[i];
+            oddIndex+=2;
+       }
+    }
+    return ans;
     }
 };
