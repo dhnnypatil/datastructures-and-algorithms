2cfd36bf88f825c4f5cf094acc2eddd11cb3887f
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2026-06-01 11:20:47 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (21.54%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        int ans = 0;
+        sort(cost.begin(), cost.end(), greater<>());
+        for(int i=2; i<cost.size(); i+=3){
+            cost[i] = 0;
+        }
+        for(int i=0; i<cost.size(); i++) ans+= cost[i];
+
+        return ans;
+    }
+};
