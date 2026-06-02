c570baf1390e4dc13f54964d181dd8b38a2fe519
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2026-06-03 01:55:25 +0530

    Time: 4 ms (64.75%) | Memory: 91.9 MB (26.23%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -4,21 +4,27 @@ public:
                            vector<int>& landDuration,
                            vector<int>& waterStartTime,
                            vector<int>& waterDuration) {
+        
+        int ans = INT_MAX;
+
         int n = landStartTime.size();
         int m = waterStartTime.size();
-        int res = INT_MAX;
+
         for (int i = 0; i < n; i++) {
-            int land = landStartTime[i] + landDuration[i];
             for (int j = 0; j < m; j++) {
-                int water = waterStartTime[j] + waterDuration[j];
 
-                int land_water = max(land, waterStartTime[j]) + waterDuration[j];
-                res = min(res, land_water);
+                // Land -> Water
+                int landFinish = landStartTime[i] + landDuration[i];
+                int waterStart = max(landFinish, waterStartTime[j]);
+                ans = min(ans, waterStart + waterDuration[j]);
 
-                int water_land = max(water, landStartTime[i]) + landDuration[i];
-                res = min(res, water_land);
+                // Water -> Land
+                int waterFinish = waterStartTime[j] + waterDuration[j];
+                int landStart = max(waterFinish, landStartTime[i]);
+                ans = min(ans, landStart + landDuration[i]);
             }
         }
-        return res;
+
+        return ans;
     }
 };
