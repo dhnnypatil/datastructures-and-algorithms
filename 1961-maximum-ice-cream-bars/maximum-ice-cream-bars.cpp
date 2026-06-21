00dc8e8c64ab6bd7036f7ffbafc88bcec0b39214
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2026-06-22 00:24:32 +0530

    Time: 11 ms (82.39%) | Memory: 87.7 MB (9.29%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(), costs.end());
+        int n = costs.size();
+        
+        int maxVal = 0;
+        for(int i = 0; i < n; i++) maxVal = max(maxVal, costs[i]);
 
-        int cnt = 0;
-        for(int i = 0; i < costs.size(); i++){
-            if(costs[i] <= coins){
-                coins -= costs[i];
-                cnt++;
-            }
+        vector<int> hash(maxVal+1, 0);
+        for(int i = 0; i < n; i++) hash[costs[i]]++;
+
+        for(int i = 1; i <= maxVal; i++) hash[i] += hash[i-1];
+
+        vector<int> sortedCosts(n);
+        for (int i = n - 1; i >= 0; i--) {
+            // Find the correct 0-indexed position and place the cost
+            sortedCosts[hash[costs[i]] - 1] = costs[i];
+            // Decrement the count so duplicate values go into the previous slot
+            hash[costs[i]]--;
         }
 
-        return cnt;
+        //Greedily buy ice cream using the fully sorted array
+        int iceCreams = 0;
+        for(int i = 0; i < n; i++){
+            if(coins >= sortedCosts[i]){
+                coins -= sortedCosts[i];
+                iceCreams++;
+            }else{
+                // Stop if we can't afford the current cheapest ice cream
+                break;
+            }
+        }
+        
+        return iceCreams;
     }
 };
