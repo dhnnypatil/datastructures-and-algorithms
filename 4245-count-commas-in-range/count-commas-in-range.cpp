04298629bb56ce3da869dbf48f781fcf8f3a328a
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2026-09-09 17:57:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (15.44%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int p = 1000, ans = 0;
+        while(p<=n){
+            ans += n-p+1;
+            p*=1000;
+        }
+        return ans;
+    }
+};
